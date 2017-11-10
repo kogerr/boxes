b8a90faff087b4c7cb3b6fab40c845b4487bc0bf
kogerr <kogerr@hotmail.com>
2017-11-10 15:10:31 +0100

    added left direction

--- slider.js
+++ slider.js
@@ -1,55 +1,59 @@
 angular.module('slider',[]).controller('sliderController', function($scope, $interval, $window){
   $scope.speed = 0;
   $scope.acc = 0;
-  $scope.pos = 0;
   $scope.deAcc = 0;
   let movement;
-  let positionChange = $interval(() => {
-    if($scope.pos > $window.innerWidth) {$scope.pos = 0;}
-    if($scope.pos < 0) {$scope.pos = $window.innerWidth;}
-    $scope.pos += $scope.speed;
-  }, 100);
+  $scope.direction = 0;
+  $scope.keyCode = '';
 
-  /*
+  let positionChange = $interval(() => {
+    if($scope.player.pos > $window.innerWidth) {$scope.player.pos = 0;}
+    if($scope.player.pos < 0) {$scope.player.pos = $window.innerWidth;}
+    $scope.player.pos += $scope.speed;
+    $scope.player.pos += $scope.speed;
+  }, 20);
+
   let slowing = $interval(() => {
-    if(!movement && Math.abs($scope.speed) > $scope.deAcc) {
-      $scope.deAcc += 2;
-      $scope.speed -= ($scope.speed > 0) ? $scope.deAcc : ($scope.deAcc * -1);
+    if(!movement || $scope.speed * $scope.direction < 0) {
+      if (Math.abs($scope.speed) > $scope.deAcc) {
+        $scope.deAcc += 2;
+        $scope.speed -= ($scope.speed > 0) ? $scope.deAcc : ($scope.deAcc * -1);
+      } else {
+        $scope.speed = 0;
+        $scope.deAcc = 0;
+      }
     }
   }, 100);
-  */
+
+  $scope.player = {pos: 0};
+  $scope.player.style = {position: 'absolute', left: this.pos + 'px', 'image-rendering': 'pixelated'};
 
   $scope.listen = function(e){
     //$scope.asdf = e.clientX;
   };
 
   $scope.keyListen = function(e){
-    if(!movement) {
+    if (e.keyCode == 37) {
+      $scope.direction = -1;
+    }
+    if (e.keyCode == 39) {
+      $scope.direction = 1;
+    }
+    if(!movement || $scope.speed * $scope.direction < 0) {
       movement = $interval(() => {
-        $scope.acc++;
+
+        $scope.acc += $scope.direction;
         $scope.speed += $scope.acc;
-      } ,100);
+      }, 100);
+      $scope.deAcc = 0;
     }
   };
 
   $scope.keyUpListen = function(e){
     $interval.cancel(movement);
+    console.log('cancelling');
+    movement = false;
     $scope.acc = 0;
-    movement = $interval(() => {
-      if($scope.speed > $scope.acc) {
-        $scope.acc+=2;
-        $scope.speed -= $scope.acc;
-      } else {
-        $scope.speed = 0;
-        $scope.acc = 0;
-        $interval.cancel(movement);
-        movement = false;
-      }
-    } ,100);
-    //movement = false;
-
-    //$scope.speed = 0;
-    //console.log(e);
-    //alert('error');
+    $scope.direction = 0;
   };
 });
